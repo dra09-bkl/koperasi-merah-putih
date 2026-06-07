c0c77b8de9d144f7e7c4d97b33e8e90cb571b8e9
rayyan <aqlyderlyrayyan@gmail.com>
2026-06-07 17:28:18 +0700

    fix 10

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <div class="flex justify-center mb-6">
+                 <img src="https://ui-avatars.com/api/?name=DER&background=8B4513&color=fff&size=150" alt="Profil Valid" style="width: 120px; height: 120px; border-radius: 50%; object-fit: cover; border: 4px solid #8B4513;">
+                    </div>
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
